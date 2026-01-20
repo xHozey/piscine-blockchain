0c23c67d0ce484692f3cbef35a36cec2f5514887
xHozey <hamzasaliheddine@hotmail.com>
2026-01-20 15:49:29 +0100

    test

---
+++ retrieve-transaction-value.js
@@ -0,0 +1,19 @@
+const BitcoinCore = require("bitcoin-core")
+
+const client = new BitcoinCore({
+    host: "127.0.0.1",
+    port: "18443",
+    username: "leeloo",
+    password: "multipass"
+})
+
+async function retrieveTransactionValue(hash) {
+    try {
+        const tran = await client.getTransactionByHash(hash)
+        return tran.amount
+    } catch(e) {
+
+    }
+}
+
+module.exports = {retrieveTransactionValue}
