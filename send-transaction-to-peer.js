7a3457ea0aef7099e5b8c5f5369c401feb1f969a
xHozey <hamzasaliheddine@hotmail.com>
2026-01-20 16:43:59 +0100

    hmm

--- send-transaction-to-peer.js
+++ send-transaction-to-peer.js
@@ -1,6 +1,6 @@
 exports.tx = {
   amount: -0.00001337,
   fee: -0.00000003,
-  txid: '95952d9bf7542dfa0c98486495f1ae432a8738bbd7da051915d0aca1bec1f9',
+  txid: '9d87d70113fcd4d1bd103430f81bb202d4dcbbc8587f4e5c7782aee768ce9b8b',
   time: 1768926060,
 }
