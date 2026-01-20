6139cb044b100d0da7bf678f81f0f9d607e93143
xHozey <hamzasaliheddine@hotmail.com>
2026-01-20 13:46:37 +0100

    test

---
+++ retrieve-block-date.js
@@ -0,0 +1,21 @@
+const BitcoinCore = require("bitcoin-core");
+
+const client = new BitcoinCore({
+  username: "leeloo",
+  password: "multipass",
+  host: "localhost",
+  port: 18443,
+  network: "regtest",
+});
+
+async function retrieveBlockDate(height) {
+  try {
+    if (typeof height === "number") {
+      blockHash = await client.getBlockHash(height);
+      const block = await client.getBlock(blockHash);
+      return block.time;
+    }
+  } catch (error) {}
+}
+
+module.exports = { retrieveBlockDate };
