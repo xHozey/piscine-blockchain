355fec3172ba1e00740cdacc9ce62be8b8a87b03
xHozey <hamzasaliheddine@hotmail.com>
2026-01-20 18:21:19 +0100

    missing 0 in one char res

--- increment.js
+++ increment.js
@@ -1,5 +1,6 @@
 const increment = (hex) => {
     let res = parseInt(hex, 16) + 1
     let hexStr = res.toString(16)
+    if (hexStr.length % 2) hexStr = '0' + hexStr
     return Buffer.from(hexStr, 'hex')
 }
