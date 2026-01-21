03f6f57258d317cc8613df07fdfc28597a207b8a
xHozey <hamzasaliheddine@hotmail.com>
2026-01-21 12:44:21 +0100

    must use pkey

--- signer.js
+++ signer.js
@@ -1,17 +1,22 @@
 const crypto = require('crypto')
+
+let privKey;
+
 const init = () => {
-    const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync('ec', {
         namedCurve: 'sect239k1'
     })
+    privKey = privateKey 
     return publicKey
 }
+
 const pubKey = init()
 
 const signer = (message) => {
     const sign = crypto.createSign('SHA256')
     sign.write(message)
     sign.end()
-    return sign.sign(pubKey, 'hex')
+    return sign.sign(privKey, 'hex')
 }
 
 const verifier = (message, pubKey, signature) => {
@@ -20,3 +25,8 @@ const verifier = (message, pubKey, signature) => {
     verify.end()
     return verify.verify(pubKey, signature, 'hex')
 }
+
+const message = "hello wolrd"
+
+const sign = signer(message)
+console.log(verifier(message, pubKey, sign))
