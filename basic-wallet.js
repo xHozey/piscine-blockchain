81f5404a2686f7fc005d91556bbfdfc67d2a1266
xHozey <hamzasaliheddine@hotmail.com>
2026-01-21 14:57:46 +0100

    test

---
+++ basic-wallet.js
@@ -0,0 +1,26 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const generateAddress = () => {
+  const { privateKey, publicKey } = crypto.generateKeyPairSync("ec", {
+    namedCurve: "secp256k1",
+    publicKeyEncoding: { type: "spki", format: "pem" },
+    privateKeyEncoding: { type: "pkcs8", format: "pem" },
+  });
+  fs.writeFileSync("wallet.pem", privateKey);
+  const publicKeyDer = crypto
+    .createPublicKey(publicKey)
+    .export({ type: "spki", format: "der" });
+  return "01" + crypto.hash("sha256", publicKeyDer);
+};
+
+const createTransaction = (amount, recipient) => {
+  let privateKey = fs
+    .readFileSync("wallet.pem")
+    .toString()
+    .replace(/-----BEGIN PRIVATE KEY-----/, "")
+    .replace(/-----END PRIVATE KEY-----/, "")
+    .replace(/\s+/g, "");
+
+  return address + recipient + amount.toString(16) + privateKey;
+};
+
