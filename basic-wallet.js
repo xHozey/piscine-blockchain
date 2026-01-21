59e9ed8f7532759d741abc73e2a686d1a24ed9a3
xHozey <hamzasaliheddine@hotmail.com>
2026-01-21 15:25:06 +0100

    derive the pub key from the privatekey

--- basic-wallet.js
+++ basic-wallet.js
@@ -11,18 +11,27 @@ const generateAddress = () => {
   const publicKeyDer = crypto
     .createPublicKey(publicKey)
     .export({ type: "spki", format: "der" });
-  return "01" + crypto.createHash("sha256").update(publicKeyDer).digest('hex');
+  return "01" + crypto.createHash("sha256").update(publicKeyDer).digest("hex");
 };
 
-let addr = generateAddress();
-console.log(addr.length);
 const createTransaction = (amount, recipient) => {
   let privateKey = fs.readFileSync("wallet.pem");
+  const publicKey = crypto
+    .createPublicKey(privateKey)
+    .export({ type: "spki", format: "pem" });
+  const publicKeyDer = crypto
+    .createPublicKey(publicKey)
+    .export({ type: "spki", format: "der" });
+
+  const sender =
+    "01" + crypto.createHash("sha256").update(publicKeyDer).digest("hex");
+
   let amountHex = amount.toString(16);
   let sign = crypto.createSign("sha256");
-  let tx = addr + recipient + amountHex;
+  let tx = sender + recipient + amountHex;
   sign.write(tx);
   sign.end();
   const signature = sign.sign(privateKey, "hex");
   return tx + signature;
 };
+
