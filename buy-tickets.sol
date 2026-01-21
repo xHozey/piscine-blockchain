49ac90819e2a36a65be296bd33de580f9d7dd76d
xHozey <hamzasaliheddine@hotmail.com>
2026-01-21 16:36:25 +0100

    the price was low

--- buy-tickets.sol
+++ buy-tickets.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.4;
 contract BuyTickets {
     mapping(address => uint) ticketsBought;
     function buyTicket() public payable {
-        require(msg.value >= 0.01 ether, "You must pay more than 0.1ETH");
+        require(msg.value >= 0.1 ether, "You must pay more than 0.1ETH");
         ticketsBought[msg.sender] += 1;
     }
 
