a771099185f126a7971ad534d1fdcfd6fab91d69
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 14:16:23 +0100

    set value 0

--- send-hash.js
+++ send-hash.js
@@ -1,19 +1,20 @@
 const ethers = require("ethers");
 const crypto = require("crypto");
-const { hexlify } = require('ethers/lib/utils')
+const { hexlify } = require("ethers/lib/utils");
 const sendHash = async (text) => {
-  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://localhost:8545"
+  );
   const hashedText = crypto.createHash("sha256").update(text).digest();
   const signer = provider.getSigner(0);
   const receiver = await provider.getSigner(1).getAddress();
   const tx = {
     to: receiver,
+    value: 0,
     data: hexlify(hashedText),
-    value: 0
-  }
-  const txRes = await signer.sendTransaction(tx)
-  txRes.wait()
-  console.log(txRes)
+  };
+  const txRes = await signer.sendTransaction(tx);
+  txRes.wait();
 };
 
 module.exports = sendHash;
