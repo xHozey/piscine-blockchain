80f7b63d679dae194da7ac0d0d7dab520d948a37
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 14:20:20 +0100

    weird

--- send-hash.js
+++ send-hash.js
@@ -1,21 +1,20 @@
 const ethers = require("ethers");
 const crypto = require("crypto");
-const { hexlify } = require("ethers/lib/utils");
 
 const sendHash = async (text) => {
   const provider = new ethers.providers.JsonRpcProvider(
     "http://localhost:8545"
   );
-  const hashedText = crypto.createHash("sha256").update(text).digest();
+  const hashedText = crypto.createHash("sha256").update(text).digest('hex');
   const signer = provider.getSigner(0);
   const receiver = await provider.getSigner(1).getAddress();
   const tx = {
     to: receiver,
     value: 0,
-    data: hexlify("0x" + hashedText),
+    data: "0x"+hashedText,
   };
   const txRes = await signer.sendTransaction(tx);
   txRes.wait();
 };
-
+sendHash("hello world")
 module.exports = sendHash;
