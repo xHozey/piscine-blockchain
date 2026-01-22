ecc978e1645cf15b32297a928a3a5d00f215b3c2
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 14:10:54 +0100

    sending to random ppl

---
+++ send-hash.js
@@ -0,0 +1,18 @@
+const ethers = require("ethers");
+const crypto = require("crypto");
+const { hexlify } = require('ethers/lib/utils')
+const sendHash = async (text) => {
+  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+  const hashedText = crypto.createHash("sha256").update(text).digest();
+  const signer = provider.getSigner(0);
+  const receiver = await provider.getSigner(1).getAddress();
+  const tx = {
+    to: receiver,
+    data: hexlify(hashedText)
+  }
+  const txRes = await signer.sendTransaction(tx)
+  txRes.wait()
+  console.log(txRes)
+};
+
+module.exports = sendHash;
