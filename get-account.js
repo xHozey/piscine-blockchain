2b1bba35abc9c22bcb98c5fbde77b46dc4dd4353
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 13:16:15 +0100

    old version ether

--- get-account.js
+++ get-account.js
@@ -1,7 +1,7 @@
 const ethers = require('ethers')
 
 const getAccount = async () => {
-    const provider = new ethers.JsonRpcApiProvider('http://localhost:8545')
+    const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
     const accounts = await provider.listAccounts()
     return accounts[0].address
 }
