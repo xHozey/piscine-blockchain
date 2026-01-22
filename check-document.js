da6cbaf73355ea5a77273368dc8af2049d0a4ff4
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 15:29:42 +0100

    test

--- check-document.js
+++ check-document.js
@@ -6,12 +6,12 @@ async function checkDocument(text, txID) {
   );
   try {
     const trans = await provider.getTransaction(txID);
-    console.log(trans);
     const hashedText =
       "0x" + crypto.createHash("sha256").update(text).digest("hex");
     if (trans.data == hashedText) {
-      return trans.timestamp.toString();
+        return trans.timestamp.toString();
     }
+    console.log(trans.data, hashedText)
     return 0;
   } catch (e) {
     return 0;
