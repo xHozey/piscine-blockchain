17a5882bd8f62da79ab53e83aa780ce8fca46399
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 15:26:52 +0100

    parse to string

--- check-document.js
+++ check-document.js
@@ -1,5 +1,5 @@
 const ethers = require("ethers");
-
+const crypto = require("crypto");
 async function checkDocument(text, txID) {
   const provider = new ethers.providers.JsonRpcProvider(
     "http://localhost:8545"
@@ -7,8 +7,10 @@ async function checkDocument(text, txID) {
   try {
     const trans = await provider.getTransaction(txID);
     console.log(trans);
-    if (trans.data == text) {
-      return trans.timestamp;
+    const hashedText =
+      "0x" + crypto.createHash("sha256").update(text).digest("hex");
+    if (trans.data == hashedText) {
+      return trans.timestamp.toString();
     }
     return 0;
   } catch (e) {
