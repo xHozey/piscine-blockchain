832471027dcddab22bcf1e83810dbd883564d59b
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 13:17:47 +0100

    deprecated

--- get-account.js
+++ get-account.js
@@ -3,7 +3,7 @@ const ethers = require('ethers')
 const getAccount = async () => {
     const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
     const accounts = await provider.listAccounts()
-    return accounts[0].address
+    return accounts[0]
 }
-
+getAccount()
 module.exports = getAccount
