dc785b47f03bcc0f8085759318b4c90ba300e778
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 13:11:03 +0100

    deprecated constructor ?

--- get-account.js
+++ get-account.js
@@ -1,7 +1,7 @@
 const ethers = require('ethers')
 
 const getAccount = async () => {
-    const provider = new ethers.JsonRpcProvider('http://localhost:8545')
+    const provider = new ethers.JsonRpcApiProvider('http://localhost:8545')
     const accounts = await provider.listAccounts()
     return accounts[0].address
 }
