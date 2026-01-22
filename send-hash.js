9a9e51d22089770f97507970ffa11d4f7c7d0248
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 14:52:15 +0100

    send to same person

--- send-hash.js
+++ send-hash.js
@@ -5,15 +5,16 @@ const sendHash = async (text) => {
   const provider = new ethers.providers.JsonRpcProvider(
     "http://localhost:8545"
   );
-  const hashedText = crypto.createHash("sha256").update(text).digest('hex');
+  const hashedText = crypto.createHash("sha256").update(text).digest("hex");
   const signer = provider.getSigner(0);
-  const receiver = await provider.getSigner(1).getAddress();
+  const receiver = await provider.getSigner(0).getAddress();
   const tx = {
     to: receiver,
-    data: "0x"+hashedText,
+    value: 0,
+    data: "0x" + hashedText,
   };
   const txRes = await signer.sendTransaction(tx);
   txRes.wait();
+  console.log(txRes)
 };
-
 module.exports = sendHash;
