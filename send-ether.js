16520dadb217480f1f69166d501ff70477a1594a
xHozey <hamzasaliheddine@hotmail.com>
2026-01-22 13:40:37 +0100

    issues with ethers version

--- send-ether.js
+++ send-ether.js
@@ -5,9 +5,9 @@ const sendEther = async (amount, address) => {
     const tx = {
         from: await provider.listAccounts()[0],
         to: address,
-        value: ethers.utils.parseEther(amount.toString())
+        value: ethers.utils.parseEther(amount.toString()).toHexString()
     }
-    const txRes = await provider.sendTransaction(tx)
+    const txRes = await provider.sendTransaction("eth_sendTransaction", [tx])
     txRes.wait()
 }
 
