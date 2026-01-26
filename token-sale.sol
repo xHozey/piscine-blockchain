40e4fe2f9c331dc50e5db50c244a6b8cf3903e4e
xHozey <hamzasaliheddine@hotmail.com>
2026-01-26 15:26:14 +0100

    test

---
+++ token-sale.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "./minimal-token.sol" as Token;
+
+contract TokenSale {
+    address minimal_token;
+    uint256 token_price;
+
+    constructor(address _minimal_token, uint256 price) {
+        minimal_token = _minimal_token;
+        token_price = price;
+    }
+
+    function buy() public payable {
+        require(msg.value >= token_price, "Not enough Ether sent");
+        uint256 tokens_to_buy = msg.value wei / token_price wei;
+        Token.MinimalToken(minimal_token).transfer(msg.sender, tokens_to_buy);
+    }
+
+    function getPrice() public view returns (uint) {
+        return token_price;
+    }
+
+    function collect() payable public {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
