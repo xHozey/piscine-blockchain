cf838f095d07a00bba0d278e148f604c84d89002
xHozey <hamzasaliheddine@hotmail.com>
2026-01-26 13:15:36 +0100

    fix typo

--- minimal-token.sol
+++ minimal-token.sol
@@ -6,16 +6,16 @@ contract MinimalToken {
     uint available_tokens;
     mapping(address => uint) balance;
 
-    contructor(uint _available_tokens, uint init_balance)  {
+    constructor(uint _available_tokens, uint init_balance)  {
         available_tokens = _available_tokens - init_balance;
         balance[msg.sender] = init_balance;
     }
 
     function balanceOf(address a) public view returns(uint) {
-        return balance[ad];
+        return balance[a];
     }
 
-    function transfer(address a, uint b) {
+    function transfer(address a, uint b) public {
         require(balance[msg.sender] >= b, "you don't have enough balance");
         balance[a] += b;
         balance[msg.sender] -= b;
