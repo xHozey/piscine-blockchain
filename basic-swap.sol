e518627790e8564931b87358647b9a892db671c4
xHozey <hamzasaliheddine@hotmail.com>
2026-01-26 16:18:09 +0100

    swap

---
+++ basic-swap.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+contract UsableToken {
+    mapping(address => uint) public accounts;
+    mapping(address => uint) public allowance;
+
+    constructor(uint256 initialNumber) {
+        accounts[msg.sender] = initialNumber;
+    }
+
+    function transfer(address to, uint256 amount) public {
+        require(accounts[msg.sender] >= amount, "Insufficient balance");
+        accounts[msg.sender] -= amount;
+        accounts[to] += amount;
+    }
+
+    function approve(address spender, uint256 amount) public {
+        allowance[spender] = amount;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public {
+        require(allowance[msg.sender] >= amount, "Allowance exceeded");
+        require(accounts[from] >= amount, "Insufficient balance");
+        allowance[msg.sender] -= amount;
+        accounts[from] -= amount;
+        accounts[to] += amount;
+    }
+}
+
+contract BasicSwap {
+    address accountA;
+    address accountB;
+
+    constructor(address a, address b) {
+        accountA = a;
+        accountB = b;
+    }
+
+    function swap(
+        address tokenA,
+        uint256 amountA,
+        address tokenB,
+        uint256 amountB
+    ) public {
+        UsableToken(tokenA).transferFrom(accountA, accountB, amountA);
+        UsableToken(tokenB).transferFrom(accountB, accountA, amountB);
+    }
+}
